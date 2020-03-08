3aef2a3672effaac9f99b0d2fd7e00442ec3f062
Robojackets-7210 <61946235+Robojackets-7210@users.noreply.github.com>
2020-03-08 19:43:40 -0400

    Add files via upload

--- robot.py
+++ robot.py
@@ -1,36 +1,60 @@
-#MADE BY FRC 7210 ROBOJACKETS. Programmers: C.J. Clos
 
-import wpilib 
+
+import wpilib
 import wpilib.drive
 
 
-class MyRobot(wpilib.IterativeRobot):
+class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
-        # Drivetrain
-
-        self.left_motor = wpilib.Spark(0)
-        self.right_motor = wpilib.Spark(1)
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.left_motor = wpilib.Spark(0)#pwm 0
+        self.right_motor = wpilib.Spark(1)#pwm 1
         self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
-        # Controller and timer
-
-        self.stick = wpilib.Joystick(1)
+        self.joystick = wpilib.Joystick(0)
         self.timer = wpilib.Timer()
-    
-    def autonomousInit(self)
-        # Reset FRC timer and start it only once every time we enter autonomous mode
+        self.forwardLimitSwitch = wpilib.DigitalInput(3)#pwm 3
+        self.reverseLimitSwitch = wpilib.DigitalInput(4)#pwm 4
+        self.arm = wpilib.Spark(5,6)#pwm 5 and 6
+      
+        self.spinner1 = wpilib.Jaguar(2)#pwm 2
+        
+       
+        self.toggle = (self.joystick, 1)
 
+   
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
         self.timer.reset()
         self.timer.start()
 
-    def autonomousPeriodic(self)
-        # We will call this function periodically durring autonomous(every 20ms?)
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
 
-        # Drive for 2 seconds
+        # Drive for two seconds
         if self.timer.get() < 2.0:
-            self.drive.arcadeDrive(-0.5, 0)  # Drive foward at half speed
+            self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
         else:
-            self.drive.arcadeDrive(0,0) # Stop!!!!!!!!!!!!
+            self.drive.arcadeDrive(0, 0)  # Stop robot
+
+    
 
     def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        self.drive.arcadeDrive(self.joystick.getY(), self.joystick.getX())
+        if self.joystick.getRawButton(5):
+            self.spinner1.set(1)
+        else:
+            self.spinner1.set(0)
+       
+        
 
+
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
